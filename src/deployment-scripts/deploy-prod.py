5c72cf3edd3a9576df2d6a84b9f9a96f83bcc0a9
Stephen Werdick <stephen.werdick@gmail.com>
2020-11-19 07:10:19 +0000

    Docker compose poc

--- src/deployment-scripts/deploy-prod.py
+++ src/deployment-scripts/deploy-prod.py
@@ -4,8 +4,7 @@ s3_client = boto3.client('s3')
 #greengrass_client = boto3.client('greengrass')
 
 S3_BUCKET_NAME = 'constellation-auto-water'
-S3_DOCKER_COMPOSE_KEY = 'docker-dependencies'
+S3_DOCKER_COMPOSE_KEY = 'pi-artifacts/docker-compose.yml'
 
 print('starting s3 upload...')
 s3_client.upload_file('../poc/docker-compose.yml', S3_BUCKET_NAME, S3_DOCKER_COMPOSE_KEY)
-
