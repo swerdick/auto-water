fbf0cf43ad64e862dae04b3e856257b678381ef7
Stephen Werdick <stephen.werdick@gmail.com>
2020-11-19 17:07:52 +0000

    Docker compose poc

--- src/deployment-scripts/deploy-prod.py
+++ src/deployment-scripts/deploy-prod.py
@@ -13,10 +13,12 @@ print('starting s3 upload...')
 s3_client.upload_file('../poc/docker-compose.yml', S3_BUCKET_NAME, S3_DOCKER_COMPOSE_KEY)
 
 print('starting greengrass deployment...')
-response=greengrass_client.create_group_version(GroupId=GREENGRASS_GROUP_ID)
+response=greengrass_client.get_group(GroupId=GREENGRASS_GROUP_ID)
+latestVersion=response['LatestVersion']
 
-greengrass_client.create_deployment(
-    DeploymentType='NewDeployment',
-    GroupId=GREENGRASS_GROUP_ID,
-    GroupVersionId=response['Version']
+response=greengrass_client.create_deployment(GroupId=GREENGRASS_GROUP_ID, 
+    DeploymentType='Redeployment', 
+    GroupVersionId=latestVersion
 )
+
+print(response)
