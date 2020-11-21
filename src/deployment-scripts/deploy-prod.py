fdfe5d984453b50f0ea0eb6379794ce97f92a4ee
Stephen Werdick <stephen.werdick@gmail.com>
2020-11-21 19:16:29 +0000

    Docker compose poc

--- src/deployment-scripts/deploy-prod.py
+++ src/deployment-scripts/deploy-prod.py
@@ -16,9 +16,36 @@ print('starting greengrass deployment...')
 response=greengrass_client.get_group(GroupId=GREENGRASS_GROUP_ID)
 latestVersion=response['LatestVersion']
 
-response=greengrass_client.create_deployment(GroupId=GREENGRASS_GROUP_ID, 
-    DeploymentType='Redeployment', 
-    GroupVersionId=latestVersion
-)
+#response=greengrass_client.create_deployment(GroupId=GREENGRASS_GROUP_ID, 
+#    DeploymentType='NewDeployment', 
+#    GroupVersionId=latestVersion
+#)
+
+response=greengrass_client.get_group_version(GroupId=GREENGRASS_GROUP_ID, GroupVersionId=latestVersion)
+
+#print(response)
+
+response=greengrass_client.create_group_version(
+    GroupId=GREENGRASS_GROUP_ID,
+    ConnectorDefinitionVersionArn=response['Definition']['ConnectorDefinitionVersionArn'],
+    CoreDefinitionVersionArn=response['Definition']['CoreDefinitionVersionArn'],
+    FunctionDefinitionVersionArn=response['Definition']['FunctionDefinitionVersionArn'],
+    LoggerDefinitionVersionArn=response['Definition']['LoggerDefinitionVersionArn'])
+
+#print(response)
+
+
+response=greengrass_client.create_deployment(
+    DeploymentType='NewDeployment',
+    GroupId=GREENGRASS_GROUP_ID,
+    GroupVersionId=response['Version'])
+    
+#print(response)
+
+response=greengrass_client.get_deployment_status(
+    GroupId=GREENGRASS_GROUP_ID,
+    DeploymentId=response['DeploymentId'])
 
 print(response)
+
+
